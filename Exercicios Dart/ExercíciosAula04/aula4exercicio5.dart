19d4387d2ccf7a97ce25ffac4d1578ca9e597824
Iagooalmeida <iago732@gmail.com>
2024-08-30 17:22:49 -0300

    feat: Adicionar função para contar palavras únicas em uma string
    Adiciona uma função no arquivo "aula4exercicio5.dart" para contar quantas palavras únicas estão presentes em uma string. A função utiliza o método `split` para separar as palavras e o método `toSet` para remover as repetições. O resultado é o número de palavras únicas na string.

--- Exercicios Dart/ExercíciosAula04/aula4exercicio5.dart
+++ Exercicios Dart/ExercíciosAula04/aula4exercicio5.dart
@@ -3,19 +3,33 @@ palavras únicas (sem repetição) estão presentes nessa string. Considere que 
 são separadas por espaços.
  */
 
+import 'dart:io';
+
 void main(){
-  String texto = "O rato roeu a roupa do rei de Roma";
-  print(contaPalavras(texto));
+  stdout.write("Digite um texto: ");
+
+  String? texto = stdin.readLineSync();
+  print(contaPalavras(texto!));
 }
 
 int contaPalavras(String texto){
   List<String> palavras = texto.split(" ");
-  List<String> palavrasUnicas = [];
-  for(var palavra in palavras){
-    if(!palavrasUnicas.contains(palavra)){
-      palavrasUnicas.add(palavra);
-    }
-  }
+
+  List<String> palavrasUnicas = palavras.toSet().toList();
 
   return palavrasUnicas.length;
 }
+
+/**Metodo 2
+ * int contaPalavras(String texto){
+  List<String> palavras = texto.split(" ");
+  // List<String> palavrasUnicas = [];
+  // for(var palavra in palavras){
+  //   if(!palavrasUnicas.contains(palavra)){
+  //     palavrasUnicas.add(palavra);
+  //   }
+  // }
+  return palavrasUnicas.length;
+}
+
+ */
